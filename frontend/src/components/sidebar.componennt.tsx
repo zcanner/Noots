420bb792751de001433791fc529a5dd3d70bddd7
Vivek Tiwari <supauservivek@gmail.com>
2024-10-24 22:07:17 +0530

    feat(sidebar): add input field for file/folder name on button click
    Added functionality to render an input field when a button is clicked.
    The input field allows users to type in a file or folder name.
    No actual file or folder creation yet, just the UI component.

--- frontend/src/components/sidebar.componennt.tsx
+++ frontend/src/components/sidebar.componennt.tsx
@@ -24,57 +24,133 @@ type FolderItemProps = {
   path: string;
 };
 
+type IsCreateState = {
+  isCreating: boolean;
+  callerType: "Folder" | "File" | null;
+};
+
 function isFileContent(
   item: FileContent | FolderStructure
 ): item is FileContent {
   return (item as FileContent).title !== undefined;
 }
 
-function SideBar(): JSX.Element {
-  const folders: FolderStructure = {
-    folder1: {
-      file1: {
-        title: "file1",
-        content: "content1",
+const folders: FolderStructure = {
+  folder1: {
+    file1: {
+      title: "file1",
+      content: "content1",
+    },
+    file2: {
+      title: "file2",
+      content: "content2",
+    },
+  },
+  folder2: {
+    file1: {
+      title: "file1",
+      content: "content1",
+    },
+    FaFolder1in2: {
+      file1infolder1in1: {
+        title: "file1infolder1in2",
+        content: "content1infolder1in2",
       },
-      file2: {
-        title: "file2",
-        content: "content2",
+      file1infolder1in2: {
+        title: "file1infolder1in2",
+        content: "content1infolder1in2",
+      },
+      file1infolder1in3: {
+        title: "file1infolder1in2",
+        content: "content1infolder1in2",
       },
     },
-    folder2: {
-      file1: {
-        title: "file1",
-        content: "content1",
-      },
-      FaFolder1in2: {
-        file1infolder1in1: {
-          title: "file1infolder1in2",
-          content: "content1infolder1in2",
-        },
-        file1infolder1in2: {
-          title: "file1infolder1in2",
-          content: "content1infolder1in2",
-        },
-        file1infolder1in3: {
-          title: "file1infolder1in2",
-          content: "content1infolder1in2",
-        },
-      },
-      FaFolder2in2: {
-        file1infolder1in2: {
-          title: "file1infolder1in2",
-          content: "content1infolder1in2",
-        },
-      },
-      file2: {
-        title: "file2",
-        content: "content2",
+    FaFolder2in2: {
+      file1infolder1in2: {
+        title: "file1infolder1in2",
+        content: "content1infolder1in2",
       },
     },
-    folder3: {},
-  };
+    file2: {
+      title: "file2",
+      content: "content2",
+    },
+  },
+  folder3: {},
+};
 
+type RenderInputFieldProps = {
+  isCreating: IsCreateState;
+  setIsCreating: React.Dispatch<React.SetStateAction<IsCreateState>>;
+};
+
+/**
+ * RenderInputField component.
+ *
+ * This component renders an input field with an icon based on the `callerType`.
+ * It is used for creating new folders or files.
+ *
+ * @param {Object} props - The props for the RenderInputField component.
+ * @param {Object} props.isCreating - The state object containing the type of the caller and the creation status.
+ * @param {string} props.isCreating.callerType - The type of the caller, either "Folder" or "File".
+ * @param {boolean} props.isCreating.isCreating - The creation status.
+ * @param {Function} props.setIsCreating - The function to update the creation status.
+ *
+ * @returns {JSX.Element} The rendered input field component.
+ */
+function RenderInputField({
+  isCreating,
+  setIsCreating,
+}: RenderInputFieldProps): JSX.Element {
+  return (
+    <div className="flex px-2 gap-2 items-center">
+      <div>
+        {isCreating.callerType === "Folder" ? <FaFolder /> : <FaFile />}
+      </div>
+      <input
+        onBlur={() => setIsCreating((prev) => ({ ...prev, isCreating: false }))}
+        autoFocus
+        type="text"
+        id="createFileFolder"
+        className="bg-transparent w-full border outline-none rounded-md"
+      />
+    </div>
+  );
+}
+
+/**
+ * SideBar component renders a sidebar for navigating and managing folders and files.
+ * It allows users to create new folders and files, and to open and close folders to view their contents.
+ */
+function SideBar(): JSX.Element {
+  const [selectedItem, setSelectedItem] = useState<string>("");
+  const [openFolders, setOpenFolders] = useState<Set<string>>(new Set());
+  const [isCreating, setIsCreating] = useState<IsCreateState>({
+    isCreating: false,
+    callerType: null,
+  });
+
+  /**
+   * Returns a function that toggles the creation state for a folder or file.
+   * It also adds the selected item to the openFolders set.
+   *
+   * @param callerType - The type of item being created ("Folder" or "File").
+   * @returns A function that handles the creation state.
+   */
+  function handleFileFolderCreation(callerType: "Folder" | "File") {
+    return (): void => {
+      if (selectedItem === "") return;
+      setIsCreating((prev) => ({ callerType, isCreating: !prev.isCreating }));
+      openFolders.add(selectedItem);
+    };
+  }
+
+  /**
+   * Sorts the folder structure such that folders appear before files and both are sorted alphabetically.
+   *
+   * @param folderStructure - The folder structure to be sorted.
+   * @returns The sorted folder structure.
+   */
   function sortFolderStructure(
     folderStructure: FolderStructure
   ): FolderStructure {
@@ -97,9 +173,13 @@ function SideBar(): JSX.Element {
 
   const sortedFolders = sortFolderStructure(folders);
 
-  const [selectedItem, setSelectedItem] = useState<string>("");
-  const [openFolders, setOpenFolders] = useState<Set<string>>(new Set());
-
+  /**
+   * FolderItem component renders a folder item and its contents.
+   * It handles the opening and closing of folders and the selection of items.
+   *
+   * @param props - The properties for the FolderItem component.
+   * @returns The JSX element for the FolderItem.
+   */
   function FolderItem({
     name,
     content,
@@ -111,6 +191,13 @@ function SideBar(): JSX.Element {
     const fullPath = path ? `${path}/${name}` : name;
     const isOpen = openFolders.has(fullPath);
 
+    /**
+     * Handles the click event for selecting an item.
+     *
+     * @param e - The mouse event.
+     * @param key - The key of the item being selected.
+     * @param type - The type of the item ("folder" or "file").
+     */
     const handleClick = (
       e: React.MouseEvent<HTMLDivElement>,
       key: string,
@@ -173,12 +260,24 @@ function SideBar(): JSX.Element {
                 );
               }
             })}
+            {isCreating.isCreating && selectedItem === fullPath && (
+              <RenderInputField
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+              />
+            )}
           </div>
         )}
       </div>
     );
   }
 
+  /**
+   * Handles the selection of an item and toggles the open state of folders.
+   *
+   * @param key - The key of the item being selected.
+   * @param type - The type of the item ("folder" or "file").
+   */
   const handleSelect = (key: string, type: "folder" | "file"): void => {
     setSelectedItem(key);
     if (type === "folder") {
@@ -208,14 +307,22 @@ function SideBar(): JSX.Element {
               <span>Notes</span>
             </div>
             <div>
-              <div className="hover:bg-gray-300 hover:bg-opacity-15 cursor-pointer p-1 rounded-full">
+              <button
+                onClick={handleFileFolderCreation("Folder")}
+                name="createFolder"
+                className="hover:bg-gray-300 hover:bg-opacity-15 cursor-pointer p-1 rounded-full"
+              >
                 <FiFolderPlus />
-              </div>
+              </button>
             </div>
             <div>
-              <div className="hover:bg-gray-300 hover:bg-opacity-15 cursor-pointer p-1 rounded-full">
+              <button
+                onClick={handleFileFolderCreation("File")}
+                name="createFile"
+                className="hover:bg-gray-300 hover:bg-opacity-15 cursor-pointer p-1 rounded-full"
+              >
                 <FiFilePlus />
-              </div>
+              </button>
             </div>
           </div>
         </h3>
